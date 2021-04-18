828fc71d8bb340b6bcfc3b2be102d3d15a129968
Pierre-Yves Lelion <pierre-yves.lelion@bnpparibas.com>
2021-04-19 00:00:13 +0200

    add allStats2csv command

--- bin/fc-game-cli.js
+++ bin/fc-game-cli.js
@@ -2,7 +2,7 @@
 
 const pkg = require('../package.json');
 const program = require('commander');
-const {ranking2csv} = require('../index.js');
+const {ranking2csv, allStats2csv} = require('../index.js');
 
 /*
 * ranking2csv command definition
@@ -21,6 +21,26 @@ program
              ranking2csv(options.period, options.game, options.filename).catch((reason)=>console.log(reason))
         }
     })
+
+/*
+* getAllStats command definition
+*/
+program
+    .command('allstats2csv')
+    .description('generate csv file containing all the stats for a pseudo')
+    .option('-f, --filename <filename>', 'output csv file')
+    .option('-n, --pseudo <pseudo>', 'the pseudo to get the stats for')
+    .option('-p, --period [period]', 'the period to get the stats for')
+    .option('-g, --game [game]', 'the game to get the stats for')
+    .action((options, command) => {
+        
+        if(!options.pseudo || !options.filename) {
+            command.outputHelp();
+        } else {
+            allStats2csv(options.pseudo,options.period, options.game, options.filename).catch((reason)=>console.log(reason))
+        }
+    })
+
 
 // Run the command
 program.parse(process.argv);
--- index.js
+++ index.js
@@ -11,6 +11,17 @@ async function getTable(month, game) {
     const result = await funcraftapi.table(month, game);
     return result;
 }
+
+/**
+ * Methode d'appel à l'API funcraft pour récupérer un classement selon les paramètres.
+ * 
+ * @param {string} pseudo le pseudo pour lequel recupere les stats 
+ */
+async function getAllStats(pseudo) {
+    const result = await funcraftapi.allStats(pseudo);
+    return result;
+}
+
 /**
  * mettre les minutes en heures + minutes
  * @param {number} minute le nombre de minute a tranformer 
@@ -65,4 +76,65 @@ async function ranking2csv(period, game, outputFile) {
         });
 }
 
+async function allStats2csv(pseudo, period, game, outputFile) {
+
+    //
+    // APPEL A L'API FUNCRAFT POUR RECUPERATION DU CLASSEMENT
+    // pour la function getTable, mettre la periode au format YYYY-MM (exemple : '2020-03')
+    //
+    const result = await getAllStats(pseudo);
+
+    let resultArray = Object.values(result).filter(p => {return typeof p==='object' && p!==null && p.always!==undefined})
+    resultArray = resultArray.flatMap(p => {return Object.values(p)}).filter(p => {return p!==null});
+
+    //
+    // ECRITURE DU FICHIER CSV RESULTAT 
+    //
+    let csvWriter;
+    if(period !== undefined && game !== undefined) {
+        resultArray = resultArray.filter(p => { return eval(`p.monthName==='${period}'`)})
+        resultArray = resultArray.filter(p => { return eval(`p.game==='${game}'`)})
+        csvWriter = createCsvWriter({
+            path: outputFile,
+            header: [
+                {id: 'rank', title: 'Rang'},
+            ],
+            headerIdDelimiter:'.'})
+    } else if(period !== undefined) {
+        resultArray = resultArray.filter(p => { return eval(`p.monthName==='${period}'`)})
+        csvWriter = createCsvWriter({
+            path: outputFile,
+            header: [
+                {id: 'game', title: 'Jeu'},
+                {id: 'rank', title: 'Rang'},
+            ],
+            headerIdDelimiter:'.'})
+    } else if(game !== undefined) {
+        resultArray = resultArray.filter(p => { return eval(`p.game==='${game}'`)})
+        csvWriter = createCsvWriter({
+            path: outputFile,
+            header: [
+                {id: 'monthName', title: 'Periode'},
+                {id: 'rank', title: 'Rang'},
+            ],
+            headerIdDelimiter:'.'})
+    } else {
+        csvWriter = createCsvWriter({
+            path: outputFile,
+            header: [
+                {id: 'game', title: 'Jeu'},
+                {id: 'monthName', title: 'Periode'},
+                {id: 'rank', title: 'Rang'},
+            ],
+            headerIdDelimiter:'.'})
+    }
+
+    csvWriter.writeRecords(resultArray)
+        .then(() => {
+            console.log(`{ code: 0, info: 'file ${outputFile} generated.'}`);
+        });
+
+}    
+
 exports.ranking2csv = ranking2csv;
+exports.allStats2csv = allStats2csv;
