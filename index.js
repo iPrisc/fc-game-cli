dd0d34a009bdab305c01018a4a520518b663eea1
Pierre-Yves Lelion <pierre-yves.lelion@bnpparibas.com>
2021-04-19 00:42:38 +0200

    comments & formatting

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 /**
  * Methode d'appel à l'API funcraft pour récupérer un classement selon les paramètres.
- * 
  * @param {string} month la periode 
  * @param {string} game le type de jeu
  */
@@ -14,7 +13,6 @@ async function getTable(month, game) {
 
 /**
  * Methode d'appel à l'API funcraft pour récupérer un classement selon les paramètres.
- * 
  * @param {string} pseudo le pseudo pour lequel recupere les stats 
  */
 async function getAllStats(pseudo) {
@@ -36,24 +34,20 @@ function convertMinute2Hour(minute) {
  * Fonction permettant de convertir le classement demandé en fichier CSV.
  *  - appel l'api funcraft pour récupérer le classement
  *  - construit le fichier csv resultat
+ * @param {String} period la periode pour laquelle recuperer le classement 
+ * @param {String} game la jeu pour laquelle recuperer le classement
+ * @param {String} outputFile le nom du fichier de sortie
  */
 async function ranking2csv(period, game, outputFile) {
     
-    //
-    // APPEL A L'API FUNCRAFT POUR RECUPERATION DU CLASSEMENT
-    // pour la function getTable, mettre la periode au format YYYY-MM (exemple : '2020-03')
-    //
+    // Appel a l'API funcraft pour recuperation du classement
+    // Pour la function getTable, mettre la periode au format YYYY-MM (exemple : '2020-03')
     const result = await getTable(period, game);
 
-    //
-    // CONVERSION DES 'MINUTES' EN 'HEURE.MINUTE'
-    //
+    // Conversion des minutes en heure.minute
     result.map(player => { player.data.gameTime = convertMinute2Hour(player.data.gameTime)})
-
     
-    //
-    // ECRITURE DU FICHIER CSV RESULTAT 
-    //
+    // Ecriture du fichier csv resultat 
     const csvWriter = createCsvWriter({
         path: outputFile,
         header: [
@@ -76,20 +70,25 @@ async function ranking2csv(period, game, outputFile) {
         });
 }
 
+/**
+ * Fonction permettant de recuperer les stats d'un joueur et de les convertir en fichier CSV.
+ *  - appel l'api funcraft pour récupérer les stats
+ *  - construit le fichier csv resultat 
+ * @param {String} pseudo le pseudo du joueur pour qui recuperer les stats 
+ * @param {String} period periode pour qui recuperer les stats
+ * @param {String} game jeu pour qui recuperer les stats
+ * @param {String} outputFile nom du fichier csv de sortie 
+ */
 async function allStats2csv(pseudo, period, game, outputFile) {
 
-    //
-    // APPEL A L'API FUNCRAFT POUR RECUPERATION DU CLASSEMENT
-    // pour la function getTable, mettre la periode au format YYYY-MM (exemple : '2020-03')
-    //
+    // Appel a l'API funcraft pour recuperation des stats
     const result = await getAllStats(pseudo);
 
+    // Reformattage du resultat
     let resultArray = Object.values(result).filter(p => {return typeof p==='object' && p!==null && p.always!==undefined})
     resultArray = resultArray.flatMap(p => {return Object.values(p)}).filter(p => {return p!==null});
 
-    //
-    // ECRITURE DU FICHIER CSV RESULTAT 
-    //
+    // Ecriture du fichier csv resultat 
     let csvWriter;
     if(period !== undefined && game !== undefined) {
         resultArray = resultArray.filter(p => { return eval(`p.monthName==='${period}'`)})
@@ -128,12 +127,10 @@ async function allStats2csv(pseudo, period, game, outputFile) {
             ],
             headerIdDelimiter:'.'})
     }
-
     csvWriter.writeRecords(resultArray)
         .then(() => {
             console.log(`{ code: 0, info: 'file ${outputFile} generated.'}`);
         });
-
 }    
 
 exports.ranking2csv = ranking2csv;
